01c418cb604a5279b177748928936ac1d53ff304
GaspardLeMarque <gleb.ushakov@yahoo.com>
2020-10-22 14:05:49 +0200

    Add hist data extraction for BTC

--- soupCMC.py
+++ soupCMC.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 import json
 
 cmc = requests.get('https://coinmarketcap.com/')
@@ -46,3 +47,36 @@ coin_list = coin_data['props']['initialState']['cryptocurrency']['listingLatest'
 coins = {}
 for i in coin_list:
     coins[str(i['id'])] = i['slug']
+
+#Save vals to the list    
+    coins_l = list(coins.values())
+for i in coins_l:
+    print(str(i))
+    
+#Check status for 10 coins (10 = limit of requests)
+ten_coins = coins_l[0:10]
+for i in ten_coins:
+    print(requests.get('https://coinmarketcap.com/currencies/' + i 
+                        + '/historical-data/?start=20130429&end=20201022').status_code)
+
+#Extract historical data
+#Only bitcoin
+page = requests.get('https://coinmarketcap.com/currencies/'+
+                        'bitcoin/historical-data/?start=20130429&end=20201022')
+#Try dfnt parsers (lxml or html.parser)
+soup = BeautifulSoup(page.text, 'lxml')
+
+#Parsing the table
+data = []
+table_body = soup.find('tbody')
+
+rows = table_body.find_all('tr')
+for row in rows:
+    cols = row.find_all('td')
+    cols = [x.text.strip() for x in cols]
+    data.append([x for x in cols if x])
+
+#Create df
+btc = pd.DataFrame(data, columns = ['Date', 'Open*', 'High', 'Low', 'Close**', 'Volume', 'Market Cap'])
+
+    
